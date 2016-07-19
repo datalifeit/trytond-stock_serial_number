9fe2daab330ac7760934349db5535c3d959d04cf
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-19 11:20:19 +0200

    Update test to version 4.0

--- tests/test_stock_serial_number.py
+++ tests/test_stock_serial_number.py
@@ -4,23 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test stock_serial_number module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_serial_number')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_serial_number')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_serial_number'
 
 
 def suite():
@@ -28,5 +19,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_stock_serial_number.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
